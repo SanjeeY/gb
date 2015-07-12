07d79babec5c3d18221996805fb507ebca0cd997
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-12 10:39:32 -0500

    kernel config

--- post.sh
+++ post.sh
@@ -1,8 +1,12 @@
-
+wget http://gentoo.mirrors.tds.net/gentoo/snapshots/portage-latest.tar.bz2
+tar -xvjpf portage-latest.tar.bz2 -C /usr
 emerge --sync
+rm portage-latest.tar.bz2
+rm stage3*
 eselect profile set 12
 emerge -uvDN @world gentoo-sources linux-firmware wpa_supplicant dhpcd wireless_tools grub
 cd /usr/src/linux
+mv /.config .
 cpucores=grep -c ^processor /proc/cpuinfo
 make -j$(cpucores)
 make install
