11e8cb21740b6614994c91e992c5624d3f53ee57
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-12 11:25:39 -0500

    minor revision

--- post.sh
+++ post.sh
@@ -2,15 +2,23 @@
 source /etc/profile
 env-update
 emerge --sync
-rm portage-latest.tar.bz2
 rm stage3*
 eselect profile set 12
-emerge -uvDN @world gentoo-sources linux-firmware wpa_supplicant dhcpcd wireless_tools grub
+echo "=sys-devel/clang-9999-r100 **" >> /etc/portage/package.accept_keywords
+echo "=sys-devel/llvm-9999 **" >> /etc/portage/package.accept_keywords
+echo "sys-devel/llvm clang" >> /etc/portage/package.use/llvm
+emerge =sys-devel/clang-9999
+export CC=clang
+export CXX=clang++
+emerge -uDN @world gentoo-sources linux-firmware wpa_supplicant dhcpcd wireless-tools grub
 cd /usr/src/linux
 mv /.config .
-cpucores=grep -c ^processor /proc/cpuinfo
+cpucores=$(grep -c ^processor /proc/cpuinfo)
 make -j$(cpucores)
+make modules_install
 make install
+export CC=gcc
+export CXX=g++
 grub2-install --target=i386-pc /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
 systemctl enable gdm
