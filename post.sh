fa04c74058c678a0d9bcdf29a4c2c899a425bce7
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-12 21:38:40 -0500

    minor revision

--- post.sh
+++ post.sh
@@ -9,7 +9,16 @@ eselect profile set 12
 echo "=sys-devel/clang-3.6.1-r100 ~amd64" >> /etc/portage/package.accept_keywords
 echo "=sys-devel/llvm-3.6.1 ~amd64" >> /etc/portage/package.accept_keywords
 echo "sys-devel/llvm clang" >> /etc/portage/package.use/llvm
-emerge =sys-devel/clang-3.6.1-r100 glibc guile autogen ntp libev recode pixman libaacplus poppler cmake xorg-server mesa ffmpeg vlc x11-libs/cairo 
+echo "media-libs/mesa ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-libs/libdrm ~amd64" >> /etc/portage/package.accept_keywords
+echo "app-eselect/eselect-opengl ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-proto/glproto ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-base/xorg-drivers ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-base/xorg-server ~amd64" >> /etc/portage/package.accept_keywords
+echo "media-libs/mesa -vaapi xa" >> /etc/portage/package.use/mesa
+echo "x11-libs/libdrm libkms" >> /etc/portage/package.use/mesa
+echo "sys-libs/zlib minizip" >> /etc/portage/package.use/zlib
+emerge =sys-devel/clang-3.6.1-r100 glibc guile autogen ntp libev recode pixman libaacplus poppler cmake xorg-server mesa ffmpeg vlc x11-libs/cairo
 export CC=clang
 export CXX=clang++
 emerge gentoo-sources linux-firmware
