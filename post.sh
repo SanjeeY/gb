aa790b936cf47d2d0f61972ae634a8cf5bc4eba1
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-12 22:06:09 -0500

    minor revision

--- post.sh
+++ post.sh
@@ -8,20 +8,10 @@ tar xfv portage-latest.tar.xz
 emerge cpuinfo2cpuflags
 cpuinfo2cpuflags-x86 >> /etc/portage/make.conf
 eselect profile set 12
-emerge -C udev
 echo "=sys-devel/clang-3.6.1-r100 ~amd64" >> /etc/portage/package.accept_keywords
 echo "=sys-devel/llvm-3.6.1 ~amd64" >> /etc/portage/package.accept_keywords
 echo "sys-devel/llvm clang" >> /etc/portage/package.use/llvm
-echo "media-libs/mesa ~amd64" >> /etc/portage/package.accept_keywords
-echo "x11-libs/libdrm ~amd64" >> /etc/portage/package.accept_keywords
-echo "app-eselect/eselect-opengl ~amd64" >> /etc/portage/package.accept_keywords
-echo "x11-proto/glproto ~amd64" >> /etc/portage/package.accept_keywords
-echo "x11-base/xorg-drivers ~amd64" >> /etc/portage/package.accept_keywords
-echo "x11-base/xorg-server ~amd64" >> /etc/portage/package.accept_keywords
-echo "media-libs/mesa -vaapi xa" >> /etc/portage/package.use/mesa
-echo "x11-libs/libdrm libkms" >> /etc/portage/package.use/mesa
-echo "sys-libs/zlib minizip" >> /etc/portage/package.use/zlib
-emerge =sys-devel/clang-3.6.1-r100 glibc guile autogen ntp libev recode pixman libaacplus poppler cmake xorg-server mesa ffmpeg vlc x11-libs/cairo
+emerge =sys-devel/clang-3.6.1-r100 glibc guile autogen ntp
 export CC=clang
 export CXX=clang++
 emerge gentoo-sources linux-firmware
@@ -37,6 +27,16 @@ sed -i -e 's/SWAP/sda2/g' /etc/fstab
 sed -i -e 's/ROOT/sda3/g' /etc/fstab
 export CC=gcc
 export CXX=g++
+echo "media-libs/mesa ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-libs/libdrm ~amd64" >> /etc/portage/package.accept_keywords
+echo "app-eselect/eselect-opengl ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-proto/glproto ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-base/xorg-drivers ~amd64" >> /etc/portage/package.accept_keywords
+echo "x11-base/xorg-server ~amd64" >> /etc/portage/package.accept_keywords
+echo "media-libs/mesa -vaapi xa" >> /etc/portage/package.use/mesa
+echo "x11-libs/libdrm libkms" >> /etc/portage/package.use/mesa
+echo "sys-libs/zlib minizip" >> /etc/portage/package.use/zlib
+emerge xorg-server mesa ffmpeg vlc x11-libs/cairo libev recode pixman libaacplus poppler cmake
 grub2-install --target=i386-pc /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
 systemctl enable sshd
