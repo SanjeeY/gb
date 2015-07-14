54370e0fe3dff7bd6294ddad404b5186a3da0118
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-14 02:28:54 -0500

    init branch

--- gscript.sh
+++ gscript.sh
@@ -1,54 +1,76 @@
 #!/bin/bash
 scriptdir=$(pwd)
-printf "This Gentoo install script requires partitions to be formatted and unmounted."
-printf "This script also requires superuser access."
-printf "The kernel config provided supports most filesystems(Ext4, XFS, Reiser4. F2FS), so root partiton can be formatted based on livecd disk utilities."
-printf "Only a three partition boot, swap, and root partition is supported at this time."
-printf "Do you wish to continue(y/n)?"
+printf "This Gentoo install script requires partitions to be formatted and unmounted.\n"
+printf "The kernel config provided supports most filesystems(Ext4, XFS, Reiser4. F2FS),\n so root partiton can be formatted based on livecd disk utilities.\n"
+printf "Only a three partition boot, swap, and root partition is supported at this time.\n"
+printf "Do you wish to continue(y/n)\n?"
 read start
 if [ "$start" == "y" ]
 then
+  #Read and mount partitions
   mkdir /mnt/gentoo
-  printf "\n\nEnter root partition device(eg: sda3)"
+  printf "\n\nEnter root partition device(e.g. sda3)\n"
   read rootPart
   mount $rootPart /mnt/gentoo
   mkdir /mnt/gentoo/boot
-  printf "\n\nEnter boot partition device(eg: sda1) - Must be ext2 for BIOS, vfat for UEFI installations."
+  printf "\n\nEnter boot partition device(e.g. sda1)\nMust be ext2 for BIOS, vfat for UEFI installations\n"
   read bootPart
   mount $bootPart /mnt/gentoo/boot
-  printf "\n\nWill there be a swap partition(y/n)?"
+  printf "\n\nWill there be a swap partition(y/n)?\n"
   read swapBool
   if [ "$swapBool" == "y" ]
   then
-    printf "\nEnter swap partition device (eg: sda2)"
+    printf "\nEnter swap partition device (eg: sda2)\n"
     read swapPart
     swapon $swapPart
   fi
+
+  #Generate random seed for mirror selection
+  mirrorSeed = $(( ( RANDOM % 10 )  + 1 ))
+  mirror = $(sed -n -e ${mirrorSeed}p mirror)
+
+  #Download and extract stage3 and portage files.
   cd /mnt/gentoo/
-  dt=$(date -d "4 day ago" +%Y%m%d)
+  dt=$(date -d "5 day ago" +%Y%m%d)
+  wget ${mirror}/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-${dt}.tar.bz2
+  wget ${mirror}/snapshots/portage-latest.tar.xz
+  mv portage-latest.tar.gz usr
+  tar xvjpf stage3*.tar.bz2 --xattrs
   rm stage3*
-  wget http://gentoo.mirrors.tds.net/gentoo/releases/amd64/autobuilds/current-stage3-amd64/stage3-amd64-${dt}.tar.bz2
-  tar xvjpf stage3-*.tar.bz2 --xattrs
-  sed -i -e 's/BOOT/$bootPart/g' /etc/fstab
+  cd usr
+  tar xfv portage-latest.tar.xz
+  rm portage-latest.tar.xz
+
+  #Update various configuration files in /etc
+  printf "\nGENTOO_MIRRORS=\"" >> etc/portage/make.conf
+  printf $mirror >> etc/portage/make.conf
+  printf "\"\n" >> etc/portage/make.conf
+  sed -i -e 's/BOOT/$bootPart/g' etc/fstab
   if [ "$swapBool" == "y" ]
   then
-    sed -i -e 's/SWAP/$swapPart/g' /etc/fstab
+    sed -i -e 's/SWAP/$swapPart/g' etc/fstab
   else
-    sed -i '/SWAP/d' /etc/fstab
+    sed -i '/SWAP/d' etc/fstab
   fi
-  sed -i -e 's/ROOT/$rootPart/g' /etc/fstab
+  sed -i -e 's/ROOT/$rootPart/g' etc/fstab
   cpucores=$(grep -c ^processor /proc/cpuinfo)
   cp ${scriptdir}/make.conf etc/portage/make.conf
   printf "MAKEOPTS=\"-j${cpucores}\"\n" >> etc/portage/make.conf
-  cp -L /etc/resolv.conf etc/
+  cp -L /etc/resolv.conf etc
+
+  #Mount necessary filesystems before chroot
   mount -t proc proc proc
   mount --rbind /sys sys
   mount --make-rslave sys
   mount --rbind /dev dev
   mount --make-rslave dev
+
+  #Copy kernel config, wifi config, post-chroot script, and other post-installation build scripts
   cp ${scriptdir}/.config .
-  cp ${scriptdir}/post.sh .
   cp ${scriptdir}/wpa_supplicant.conf .
+  cp ${scriptdir}/post.sh .
   cp -R ${scriptdir}/buildScripts .
+
+  #Enter chroot and execute post.sh
   chroot /mnt/gentoo ./post.sh
 fi
--- post.sh
+++ post.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 source /etc/profile
 env-update
-rm stage3*
-cd /usr
-wget http://gentoo.mirrors.tds.net/gentoo/snapshots/portage-latest.tar.xz
-tar xfv portage-latest.tar.xz
+
+#Add CPU processor flags for builds such as ffmpeg
 emerge cpuinfo2cpuflags
 cpuinfo2cpuflags-x86 >> /etc/portage/make.conf
 printf "\n" >> /etc/portage/make.conf
-eselect profile set 12
+
+#Build and switch to clang. Also build some packages with gcc that break with clang.
 printf "=sys-devel/clang-3.6.1-r100 ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "=sys-devel/llvm-3.6.1 ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "sys-devel/llvm clang\n" >> /etc/portage/package.use/llvm
 emerge =sys-devel/clang-3.6.1-r100 glibc guile autogen ntp
 export CC=clang
 export CXX=clang++
+
+#Download and build kernel. Uses included kernel config file from git.
 emerge gentoo-sources linux-firmware
 cd /usr/src/linux
 mv /.config .
@@ -22,21 +23,29 @@ cpucores=$(grep -c ^processor /proc/cpuinfo)
 make -j${cpucores}
 make modules_install
 make install
-emerge -uDN @world  wpa_supplicant dhcpcd wireless-tools grub
 
+#Selects vanilla systemd profile. Builds systemd, bootloader, some net tools and a world update.
+eselect profile set 12
+emerge -uDN @world  wpa_supplicant dhcpcd wireless-tools grub
 grub2-install --target=i386-pc /dev/sda
 grub2-mkconfig -o /boot/grub/grub.cfg
+
+#Enables ssh, dhcpcd, and ntp.
 systemctl enable sshd
 systemctl enable dhcpcd
 systemctl enable ntpd
+
+#Root password prompt
 printf "\nPlease enter root password:\n"
 passwd
+
+#WPA_Supplicant configuration
 printf "\nWould you like to set up wifi essid/key(y/n)?"
 read wifiBool
 if [ "$wifiBool" == "y" ];
 then
   $wifiSetup=0
-  while[ "$wifiSetup" == "0" ];
+  while[ "$wifiSetup" == "0" ]
   do
     printf "\nEnter wifi ssid:"
     read wifiSSID
@@ -53,6 +62,8 @@ then
   sed -i -e 's/KEYVAR/$wifiKEY/g' /wpa_supplicant.conf
   mv /wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 fi
+
+#Xorg-server + desktop environment build scripts
 printf "\nDo you want to install xorg-server?"
 read xorg
 if ["$xorg" == "y"];
