11f3e6956b0ecdeaa1e643d579c8ffecdc909bfe
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-14 02:44:24 -0500

    minor revision

--- gscript.sh
+++ gscript.sh
@@ -11,18 +11,18 @@ then
   mkdir /mnt/gentoo
   printf "\n\nEnter root partition device(e.g. sda3)\n"
   read rootPart
-  mount $rootPart /mnt/gentoo
+  mount /dev/$rootPart /mnt/gentoo
   mkdir /mnt/gentoo/boot
   printf "\n\nEnter boot partition device(e.g. sda1)\nMust be ext2 for BIOS, vfat for UEFI installations\n"
   read bootPart
-  mount $bootPart /mnt/gentoo/boot
+  mount /dev/$bootPart /mnt/gentoo/boot
   printf "\n\nWill there be a swap partition(y/n)?\n"
   read swapBool
   if [ "$swapBool" == "y" ]
   then
     printf "\nEnter swap partition device (eg: sda2)\n"
     read swapPart
-    swapon $swapPart
+    swapon /dev/$swapPart
   fi
 
   #Generate random seed for mirror selection
