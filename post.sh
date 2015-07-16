563861e22877a4dd3e1753da2379e514c2ec3be9
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-16 03:37:42 -0500

    m

--- post.sh
+++ post.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+startH=$(date '+%H')
+startM=$(date '+%M')
+startS=$(date '+%S')
 {
 source /etc/profile
 env-update
+emerge--sync
 
 mkdir /etc/wpa_supplicant
 mv wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
@@ -45,7 +49,7 @@ emerge -uDN @world wpa_supplicant dhcpcd wireless-tools p7zip dev-tcltk/expect g
 #Enables ssh, dhcpcd, and ntp.
 systemctl enable sshd
 systemctl enable dhcpcd
-systemctl enable ntpd
+
 
 #Update config files
 etc-update --automode -3
@@ -78,6 +82,16 @@ printf "[F3] Archiving installation"
 printf "======================================================================="
 XZ_OPT=-9 tar -cvpJf /backup/backup.cinnamon.tar.xz --directory=/ --exclude=proc --exclude=sys --exclude=dev/pts --exclude=backup .
 
-
+systemctl enable ntpd
 exit
-} 2>&1 | while IFS= read -r line; do printf '[%s] %s\n' "$(date '+%H:%M:%S')" "$line"; done | tee -a post.log
+} 2>&1 |
+while IFS= read -r line;
+do
+newH=$(date '+%H')
+newM=$(date '+%M')
+newS=$(date '+%S')
+fH=$((newH-startH))
+fM=$((newM-startM))
+fS=$((newS-startS))
+printf  "\[$fH\:$fM\:$fS\]  $line";
+done | tee -a post.log
