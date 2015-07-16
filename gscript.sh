1d03259924f1151b9d6da309301ca2c57a782185
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-16 00:59:06 -0500

    .

--- gscript.sh
+++ gscript.sh
@@ -59,4 +59,4 @@ cp ${scriptdir}/boot/* boot
 
 #Enter chroot and execute post.sh
 chroot /mnt/gentoo ./post.sh
-} 2>&1 | tee -a gscript.log
+} 2>&1 | python -c 'import sys,time;sys.stdout.write("".join(( " ".join((time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()), line)) for line in sys.stdin )))' | tee -a gscript.log
