0bbf26d0f11ad460f4e78b8bb2d7992610336a07
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-07-16 09:29:47 -0500

    a

--- post.sh
+++ post.sh
@@ -21,20 +21,20 @@ printf "sys-kernel/gentoo-sources ~amd64\n" >> /etc/portage/package.accept_keywo
 printf "sys-devel/llvm clang\n" >> /etc/portage/package.use/llvm
 printf "media-libs/harfbuzz icu\n" >> /etc/portage/package.use/llvm
 printf "sys-apps/systemd gudev\n" >> /etc/portage/package.use/llvm
-printf "[1] Building LLVM & Clang\n"
+printf "[1.] Building LLVM & Clang\n"
 printf "======================================================================="
 emerge clang guile autogen ntp
 export CC=clang
 export CXX=clang++
 
 #Download and build kernel. Uses included kernel config file from git.
-printf "[2] Building kernel [clang enabled]"
+printf "[2.] Building kernel [clang enabled]"
 printf "======================================================================="
 
 emerge gentoo-sources linux-firmware
 cd /usr/src/linux
 openssl req -new -nodes -utf8 -sha512 -days 36500 -batch -x509 -config /buildScripts/x509.genkey -outform DER -out signing_key.x509 -keyout signing_key.priv
-mv /.config .
+cp /.config .
 cpucores=$(grep -c ^processor /proc/cpuinfo)
 make oldconfig
 make -j${cpucores}
@@ -44,9 +44,10 @@ make install
 #cp /usr/src/linux/arch/arm/boot/zImage /boot/kernel7.img
 
 #Selects vanilla systemd profile. Builds systemd, bootloader, some net tools and a world update.
-printf "[3] Updating world and installing various network utilities [clang enabled]"
+printf "[3.] Updating world and installing various network utilities [clang enabled]"
 printf "======================================================================="
 eselect profile set 12
+emerge -C udev
 emerge -uDN @world wpa_supplicant dhcpcd wireless-tools p7zip dev-tcltk/expect grub
 
 #Enables ssh, dhcpcd, and ntp.
@@ -64,24 +65,24 @@ etc-update --automode -3
 ./setp.sh
 mkdir /backup
 
-printf "[F1] Archiving installation"
+printf "[F1.] Archiving installation"
 printf "======================================================================="
 XZ_OPT=-9 tar -cvpJf /backup/backup.tar.xz --directory=/ --exclude=proc --exclude=sys --exclude=dev/pts --exclude=backup .
 
-printf "[4] Building xorg-server"
+printf "[4.] Building xorg-server"
 printf "======================================================================="
 ./buildScripts/xorg.sh
 
-printf "[F2] Archiving installation"
+printf "[F2.] Archiving installation"
 printf "======================================================================="
 XZ_OPT=-9 tar -cvpJf /backup/backup.xorg-server.tar.xz --directory=/ --exclude=proc --exclude=sys --exclude=dev/pts --exclude=backup .
 
 
-printf "[5] Building Cinnamon"
+printf "[5.] Building Cinnamon"
 printf "======================================================================="
 ./buildScripts/buildCinnamon.sh
 
-printf "[F3] Archiving installation"
+printf "[F3.] Archiving installation"
 printf "======================================================================="
 XZ_OPT=-9 tar -cvpJf /backup/backup.cinnamon.tar.xz --directory=/ --exclude=proc --exclude=sys --exclude=dev/pts --exclude=backup .
 
