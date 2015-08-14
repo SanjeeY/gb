53f00bcdd0156da850c1f716b2bb25c233c777a4
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-08-14 17:16:49 -0500

    added kernel config

--- buildScripts/xorg.sh
+++ buildScripts/xorg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's/USE="/USE="X dbus pulseaudio gudev /' /etc/portage/make.conf
+sed -i 's/USE="/USE="X icu egl dbus pulseaudio gudev /' /etc/portage/make.conf
 printf "media-libs/mesa ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "x11-libs/libdrm ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "app-eselect/eselect-opengl ~amd64\n" >> /etc/portage/package.accept_keywords
@@ -7,10 +7,11 @@ printf "x11-proto/glproto ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "x11-base/xorg-drivers ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "x11-base/xorg-server ~amd64\n" >> /etc/portage/package.accept_keywords
 printf "media-libs/mesa -vaapi xa\n" >> /etc/portage/package.use/mesa
+printf "x11-apps/mesa-progs -egl\n" >> /etc/portage/package.use/mesa
 printf "x11-libs/libdrm libkms\n" >> /etc/portage/package.use/mesa
 printf "x11-libs/cairo opengl\n" >> /etc/portage/package.use/mesa
 printf "sys-libs/zlib minizip\n" >> /etc/portage/package.use/zlib
 printf "x11-drivers/xf86-video-r128\n" >> /etc/portage/package.unmask
-emerge xorg-server mesa x11-libs/cairo libev pixman
+emerge xorg-server mesa x11-libs/cairo libev pixman xterm twm
 emerge libdrm libXdamage libXxf86vm xinit qtgui
 emerge ffmpeg vlc recode libaacplus poppler app-crypt/gnupg
