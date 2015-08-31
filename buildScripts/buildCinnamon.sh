6ee33664cf09fd6fb543a9b8c83a17c81d1e88a0
Sanjee <sanjee@Sanjees-MBP.fios-router.home>
2015-08-31 14:37:19 -0500

    build scripts modified

--- buildScripts/buildCinnamon.sh
+++ buildScripts/buildCinnamon.sh
@@ -3,5 +3,4 @@ sed -i 's/USE="/USE="gtk /' /etc/portage/make.conf
 printf "x11-libs/gtk+ X\n" >> /etc/portage/package.use/webkitgtk
 printf "net-libs/webkit-gtk -gles2\n" >> /etc/portage/package.use/webkitgtk
 printf "virtual/notification-daemon gnome\n" >> /etc/portage/package.use/webkitgtk
-emerge notification-daemon
 emerge cinnamon gdm
