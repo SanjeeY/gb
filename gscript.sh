a77f1d509245df6a3de5fa33bce9f10bcbb9f090
Sanjee <sanjee.yogeswaran@gmail.com>
2016-10-20 08:26:56 -0500

    removed sha check

--- gscript.sh
+++ gscript.sh
@@ -30,38 +30,6 @@ wget ${mirror}releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd6
 wget ${mirror}snapshots/portage-latest.tar.xz
 wget ${mirror}snapshots/portage-latest.tar.xz.md5sum
 
-stageTSig=$(awk '/SHA/{getline; print}' stage3-amd64-systemd-${version}.tar.bz2.DIGESTS.asc | awk 'NR==2{print $1;}')
-echo $stageTSig
-stageDSig=$(sha512sum stage3-amd64-systemd-${version}.tar.bz2 | awk '{print $1}')
-echo $stageDSig
-portageTSig=$(md5sum portage-latest.tar.xz)
-portageDSig=$(grep xz portage-latest.tar.xz.md5sum)
-echo $portageTSig
-echo $portageDSig
-#Download and extract stage3 and portage files.
-while [[ "$stageTSig" != "$stageDSig" || $portageTSig != $portageDSig ]];
-do
-{
-  rm stage3*
-  rm portage*
-  mirrorSeed=$((($(date +%s)%${numMirrors})+1))
-  mirror=$(sed -n -e ${mirrorSeed}p mirrors)
-  wget ${mirror}releases/amd64/autobuilds/latest-stage3-amd64-systemd.txt
-  version=$(sed -n -e 3p latest-stage3-amd64-systemd.txt | grep -o '^\S*' |  cut -d \/ -f 1)
-  wget ${mirror}releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-${version}.tar.bz2
-  wget ${mirror}releases/amd64/autobuilds/current-stage3-amd64-systemd/stage3-amd64-systemd-${version}.tar.bz2.DIGESTS.asc
-  wget ${mirror}snapshots/portage-latest.tar.xz
-  wget ${mirror}snapshots/portage-latest.tar.xz.md5sum
-  stageTSig=$(awk '/SHA/{getline; print}' stage3-amd64-systemd-${version}.tar.bz2.DIGESTS.asc | awk 'NR==2{print $1;}')
-  stageDSig=$(sha512sum stage3-amd64-systemd-${version}.tar.bz2 | awk '{print $1}')
-  portageTSig=$(md5sum portage-latest.tar.xz)
-  portageDSig=$(grep xz portage-latest.tar.xz.md5sum)
-  echo $stageTSig
-  echo $stageDSig
-  echo $portageTSig
-  echo $portageDSig
-}
-done
 
 printf "Extracting stage3...\n"
 tar xvjpf stage3*.tar.bz2 --xattrs
